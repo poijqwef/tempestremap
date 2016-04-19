bfc741d930155cdd840a8dd94a4d128d89016a5f
poijqwef <drosa@lbl.gov>
2016-04-19 15:45:42 -0700

    it runs at nersc

--- runMeAtNersc.sh
+++ runMeAtNersc.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+module load cray-libsci
+
 [ 0 -eq 1 ] && echo ./hdf5-netcdf.sh
 libDir=`pwd`/Local
 
 codeDir=`pwd`
 cd $codeDir
-export CPP_COMPILER=CC
-export C_COMPILER=cc
-export CPPFLAG="-mkl -std=c++0x"
-export CFLAGS="-mkl -std=c++0x"
-export LDFILES="-mkl"
-#export LDFILES="-mkl -lblas --llapack"
+export CPP_COMPILER=g++
+export C_COMPILER=gcc
+export CFLAGS="-std=c++0x"
 
+export LDFILES="-L$CRAY_LIBSCI_PREFIX_DIR/lib -lsci_intel"
+
+make clean
 make NETCDF_LIB=$libDir/lib NETCDF_INC=$libDir/include
 
